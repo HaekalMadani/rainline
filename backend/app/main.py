83f688a5c79438216fee60f8a12a16786caaeae3
Haekal-Mac <haekal.madani@gmail.com>
2025-10-09 09:33:58 +0900

    .

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
 from app.api import season
 
-app = FastAPI(
-    title="F1 Wet Weather Performance API",
-    description="An API to analyze F1 driver performance in wet conditions."
-)
+app = FastAPI()
 
 app.include_router(
     season.router,
